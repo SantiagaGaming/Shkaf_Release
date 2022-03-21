3e93c821b06c5c7d91d9193b16f7d699e902a5ef
Ilya <ganychev92@gmail.com>
2022-03-21 13:18:11 +0300

    small tumbler added

---
+++ Assets/Scripts/InteractabaleObjects/SmallTumblerObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallTumblerObject : BaseObject
+{
+    [SerializeField] private GameObject _temblers;
+    public override void StartAction()
+    {
+
+        StartCoroutine(RotateTumbler(true));
+    }
+    public override void RevertAction()
+    {
+        StartCoroutine(RotateTumbler(false));
+    }
+    private IEnumerator RotateTumbler(bool value)
+    {
+        canAction = false;
+        if (value)
+        {
+            int x = 0;
+            while (x <= 105)
+            {
+                _temblers.transform.localRotation = Quaternion.Euler(0, 0, x);
+                x++;
+                yield return new WaitForSeconds(0.01f);
+
+            }
+        }
+        else
+        {
+            int x = 105;
+            while (x >= 0)
+            {
+                _temblers.transform.localRotation = Quaternion.Euler(0, 0, x);
+                x--;
+                yield return new WaitForSeconds(0.01f);
+
+            }
+        }
+        canAction = true;
+    }
+}
